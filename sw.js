3478e9c548c76ff7d6f77940da28be1753884bed
hofe0725-beep <hofe0725@gmail.com>
2026-07-30 21:17:13 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,23 +1,21 @@
 const CACHE = 'honeymoon-v1';
 const ASSETS = [
-  '/index.html',
-  '/manifest.json',
-  '/icon-192.png',
-  '/icon-512.png',
-  'https://fonts.googleapis.com/css2?family=Noto+Serif+KR:wght@300;400&family=Noto+Sans+KR:wght@300;400;500;700&display=swap'
+  '/Hochiri/',
+  '/Hochiri/index.html',
+  '/Hochiri/manifest.json',
+  '/Hochiri/icon-192.png',
+  '/Hochiri/icon-512.png'
 ];
 
-// 설치: 파일 캐시
 self.addEventListener('install', e => {
   e.waitUntil(
-    caches.open(CACHE).then(cache => {
-      return Promise.allSettled(ASSETS.map(url => cache.add(url).catch(() => {})));
-    })
+    caches.open(CACHE).then(cache =>
+      Promise.allSettled(ASSETS.map(url => cache.add(url).catch(()=>{})))
+    )
   );
   self.skipWaiting();
 });
 
-// 활성화: 이전 캐시 삭제
 self.addEventListener('activate', e => {
   e.waitUntil(
     caches.keys().then(keys =>
@@ -27,7 +25,6 @@ self.addEventListener('activate', e => {
   self.clients.claim();
 });
 
-// 요청 처리: 캐시 우선, 없으면 네트워크
 self.addEventListener('fetch', e => {
   e.respondWith(
     caches.match(e.request).then(cached => {
@@ -35,9 +32,9 @@ self.addEventListener('fetch', e => {
       return fetch(e.request).then(res => {
         if (!res || res.status !== 200 || res.type === 'opaque') return res;
         const clone = res.clone();
-        caches.open(CACHE).then(cache => cache.put(e.request, clone));
+        caches.open(CACHE).then(c => c.put(e.request, clone));
         return res;
-      }).catch(() => caches.match('/index.html'));
+      }).catch(() => caches.match('/Hochiri/index.html'));
     })
   );
 });
